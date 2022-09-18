e22edb52d2c24144ccedbf526edc683945706680
Egii <regidesoftian@gmail.com>
2022-09-18 09:19:51 +0000

    merlinx: PixelOS-ify
    Signed-off-by: Egii <regidesoftian@gmail.com>
    Change-Id: I6754f849d140f1b97b64dab82c7a2b266655c1aa

--- aosp_merlinx.mk
+++ aosp_merlinx.mk
@@ -15,6 +15,7 @@
 #
 
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2340
 
@@ -27,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 #
 # All components inherited here go to system_ext image
@@ -37,7 +38,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlinx
-PRODUCT_NAME := lineage_merlinx
+PRODUCT_NAME := aosp_merlinx
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 
